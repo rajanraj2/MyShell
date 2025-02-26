3acd1d86bea2ddfca8054a6633270f2e2a8d23c0
rajanraj2 <rajanraj253545@gmail.com>
2025-02-26 13:48:48 +0530

    handle invalid commands

---
+++ src/try.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// double log(double x) {  
+//     return x * 2;
+// }
+
+int main() {
+    std::cout << log(10) << std::endl;  // Unambiguous
+    return 0;
+}
