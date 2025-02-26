fda1fa3f89a400d3681298f3ae7d4ef13c28d8c9
rajanraj2 <rajanraj253545@gmail.com>
2025-02-26 12:25:56 +0000

    Check executable at path

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,37 @@
 #include <sstream>
 #include <string>
 #include <unordered_set>
+#include <vector>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+void executable_files(std::stringstream path, std::string command) {
+  std::string directory;
+  while (std::getline(path, directory, ':')) {
+    // search at this directory for the required command, if it is found then print the path and return
+    for (auto file : fs::directory_iterator(directory)) {
+      if (file.path().filename() == command) {
+        std::string value = file.path();
+        std::cout << "command is " << value  << "\n";
+        return;
+      }
+    }
+  }
+  std::cout << command << ": not found\n";
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
  
-  // Uncomment this block to pass the first stage
   std::cout << "$ ";
 
   std::string input;
   std::unordered_set<std::string> builtin_commands = {"echo", "type", "exit"};
+
+  std::string path = std::getenv("PATH");
   
   while (true) {
     std::getline(std::cin, input);
@@ -23,16 +43,11 @@ int main() {
     iss >> command;
     std::getline(iss >> std::ws, statement);
 
-    if (command == "echo") {
-      std::cout << statement << "\n";
-    }
+
+    if (command == "echo") std::cout << statement << "\n";
     else if (command == "type") {
-      if (builtin_commands.find(statement) != builtin_commands.end()) {
-        std::cout << statement << " is a shell builtin\n";
-      }
-      else {
-        std::cout << statement << ": not found\n";
-      }
+      if (builtin_commands.find(statement) != builtin_commands.end()) std::cout << statement << " is a shell builtin\n";
+      else executable_files(std::stringstream(path), statement);
     }
     else std::cout << input << ": command not found\n";
     
